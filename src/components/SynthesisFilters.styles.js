8ad2290d6de16dc9cca7a90fde61ebe9703c3ce4
German1138 <germancourvoisier1138@gmail.com>
2024-11-27 19:12:17 -0300

    feat: :construction: Filters for the synthesis
    Right now they don't have any functionality, it's just the look and feel with hardcoded information.

---
+++ src/components/SynthesisFilters.styles.js
@@ -0,0 +1,12 @@
+export const container = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
+  margin: '15px 0px',
+};
+
+export const btnStyles = {
+  color: '#000000',
+  borderRadius: '10px',
+  textTransform: 'capitalize',
+};
---
+++ src/components/SynthesisFilters.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Box, Button } from '@mui/material';
+import React from 'react';
+
+import WidgetsRoundedIcon from '@mui/icons-material/WidgetsRounded';
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
+import MemoryRoundedIcon from '@mui/icons-material/MemoryRounded';
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
+import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
+import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
+
+import { useState } from 'react';
+import { container, btnStyles } from './SynthesisFilters.styles';
+
+function SynthesisFilters() {
+  let [active, setActive] = useState('for-you');
+
+  const buttonData = [
+    { name: 'for-you', label: 'For you', icon: <WidgetsRoundedIcon /> },
+    { name: 'top', label: 'Top', icon: <StarBorderRoundedIcon /> },
+    {
+      name: 'tech-science',
+      label: 'Tech & Science',
+      icon: <MemoryRoundedIcon />,
+    },
+    { name: 'finance', label: 'Finance', icon: <PaidOutlinedIcon /> },
+    {
+      name: 'arts-culture',
+      label: 'Arts & Culture',
+      icon: <ColorLensOutlinedIcon />,
+    },
+    { name: 'sports', label: 'Sports', icon: <SportsSoccerOutlinedIcon /> },
+    { name: 'health', label: 'Health', icon: <FavoriteBorderOutlinedIcon /> },
+    { name: 'education', label: 'Education', icon: <SchoolOutlinedIcon /> },
+    { name: 'food', label: 'Food', icon: <FastfoodOutlinedIcon /> },
+    { name: 'fashion', label: 'Fashion', icon: <CheckroomOutlinedIcon /> },
+  ];
+
+  return (
+    <Box sx={container}>
+      {buttonData.map(({ name, label, icon }, index) => (
+        <Button
+          key={index}
+          variant={active === name ? 'contained' : 'text'}
+          onClick={() => setActive(name)}
+          startIcon={icon}
+          sx={{
+            ...btnStyles,
+            ...(active === name && { backgroundColor: '#55D6BE' }),
+          }}
+        >
+          {label}
+        </Button>
+      ))}
+    </Box>
+  );
+}
+
+export default SynthesisFilters;
