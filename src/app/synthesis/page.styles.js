50933d91e4e684b5e1b65de286f14f422accc492
German1138 <germancourvoisier1138@gmail.com>
2025-01-14 17:51:25 -0300

    style: :lipstick: Synthesis page styles

---
+++ src/app/synthesis/page.styles.js
@@ -0,0 +1,15 @@
+export const container = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '40px',
+};
+export const subContainer = {
+  display: 'flex',
+  gap: '40px',
+};
+export const sectionsContainer = {
+  display: 'flex',
+  gap: '100px',
+  flexDirection: 'column',
+  width: { xs: '100%', md: '85%' },
+};
