211f53bd7858a6de73cda0032d3c94f5aced4422
German1138 <germancourvoisier1138@gmail.com>
2025-01-21 19:24:08 -0300

    refactor: :recycle: Incorporated THEME_SECTION_DATA constant data into SYNTHESIS_DATA

--- src/constants/SYNTHESIS_DATA.js
+++ src/constants/SYNTHESIS_DATA.js
@@ -6,12 +6,24 @@ const SYNTHESIS_DATA = {
       title: 'Positive relationships',
       description:
         'The importance of nurturing positive relationships with family, friends, and colleagues to enhance overall happiness.',
+      quote: {
+        text: '“Good relationships lead to health and happiness. The trick is that those relationships must be nurtured. ”',
+        expertName: 'Sarah Lee',
+        profession: 'Data Scientist',
+        image: 'sarah-lee.png',
+      },
       content: [
         {
           id: 1,
           subtitle: 'The importance of Family Relationships',
           description:
             'Family relationships are foundational to individual happiness and well-being. The Harvard Study of Adult Development emphasizes that strong family bonds significantly contribute to long-term health and happiness. These relationships provide emotional support, shared experiences, and a sense of belonging. During challenging times, such as the pandemic, the support from family becomes even more crucial. Nurturing these relationships requires effort, patience, and understanding, but the rewards in terms of happiness and life satisfaction are immense.',
+          quote: {
+            text: '“Happiness is love. Full stop.”',
+            expertName: 'Jane Smith',
+            profession: 'Product Manager',
+            image: 'jane-smith.png',
+          },
         },
         {
           id: 2,
@@ -32,64 +44,13 @@ const SYNTHESIS_DATA = {
       title: 'Physical Health',
       description:
         'The importance of nurturing positive relationships with family, friends, and colleagues to enhance overall happiness.',
+
       content: [
         {
           id: 1,
           subtitle: 'Exercise and Happiness',
           description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-        {
-          id: 2,
-          subtitle: 'Diet and Mental Health',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-        {
-          id: 3,
-          subtitle: 'Sleep and Emotional Well-being',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-      ],
-    },
-    {
-      id: 3,
-      title: 'Physical Health',
-      description:
-        'The importance of nurturing positive relationships with family, friends, and colleagues to enhance overall happiness.',
-      content: [
-        {
-          id: 1,
-          subtitle: 'Exercise and Happiness',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-        {
-          id: 2,
-          subtitle: 'Diet and Mental Health',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-        {
-          id: 3,
-          subtitle: 'Sleep and Emotional Well-being',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
-        },
-      ],
-    },
-    {
-      id: 4,
-      title: 'Physical Health',
-      description:
-        'The importance of nurturing positive relationships with family, friends, and colleagues to enhance overall happiness.',
-      content: [
-        {
-          id: 1,
-          subtitle: 'Exercise and Happiness',
-          description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
         },
         {
           id: 2,
