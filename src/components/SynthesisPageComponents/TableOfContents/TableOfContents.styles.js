89674318ec3294cc696187c17ce8af8a269ef970
German1138 <germancourvoisier1138@gmail.com>
2025-01-21 18:57:33 -0300

    style: :lipstick: Table of contents styles

---
+++ src/components/SynthesisPageComponents/TableOfContents/TableOfContents.styles.js
@@ -0,0 +1,42 @@
+export const container = {
+  display: { xs: 'none', md: 'block' },
+  bgcolor: 'background.paper',
+  maxWidth: '375px',
+
+  zIndex: 1300,
+  padding: 0,
+  fontFamily: 'var(--font-roboto)',
+};
+
+export const listContainer = {
+  fontWeight: '700',
+  fontSize: '25px',
+  color: '#000000',
+  maxHeight: '450px',
+  overflow: 'auto',
+};
+
+export const tableOfContentsLabel = {
+  fontWeight: '700',
+  fontSize: '25px',
+  padding: '20px 0',
+};
+
+export const contentContainer = {
+  borderLeft: '3px solid #343967 ',
+  marginBottom: '20px',
+};
+
+export const titleStyle = {
+  fontWeight: '600',
+  fontSize: '17px',
+  padding: '0px 0px 15px 20px',
+};
+
+export const subTitleStyle = {
+  padding: 0,
+  paddingLeft: 2,
+  marginLeft: 4,
+  borderLeft: '3px solid #343967 ',
+  marginBottom: '20px',
+};
