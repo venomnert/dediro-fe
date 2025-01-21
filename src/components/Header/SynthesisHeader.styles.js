d136d8d2c5f4ca4b169164dd96122bd1fe3b9b67
German1138 <germancourvoisier1138@gmail.com>
2025-01-21 19:21:42 -0300

    fix: :lipstick: Item alignment fixed

--- src/components/Header/SynthesisHeader.styles.js
+++ src/components/Header/SynthesisHeader.styles.js
@@ -11,9 +11,8 @@ export const logo = { maxWidth: '63px', width: '100%', color: 'blue' };
 
 export const buttonsContainer = {
   display: 'flex',
-  maxWidth: '300px',
   alignItems: 'center',
-  width: { xs: 'fit-content', ml: 'fit-content', lg: '100%' },
+  width: 'fit-content',
 };
 
 export const button = {
