a4741ef084dcee2b72ec44115824fd7d7b3b5028
German1138 <germancourvoisier1138@gmail.com>
2025-01-21 18:42:52 -0300

    style: :fire: Removed translate button styles

--- src/components/Header/SynthesisHeader.styles.js
+++ src/components/Header/SynthesisHeader.styles.js
@@ -1,6 +1,7 @@
 export const container = {
   display: 'flex',
   flexDirection: 'row',
+  gap: '20px',
   justifyContent: 'space-between',
   padding: '20px',
   borderBottom: '2px solid #000000',
@@ -10,28 +11,13 @@ export const logo = { maxWidth: '63px', width: '100%', color: 'blue' };
 
 export const buttonsContainer = {
   display: 'flex',
-  flexDirection: 'row',
   maxWidth: '300px',
   alignItems: 'center',
-  justifyContent: { xs: 'right', ml: 'right', lg: 'space-between' },
   width: { xs: 'fit-content', ml: 'fit-content', lg: '100%' },
 };
 
-export const iconButton = {
-  borderRadius: '100%',
-  height: '50px',
-  width: '50px',
-  backgroundColor: '#343967',
-  ':hover': {
-    background: '#343967',
-    filter: 'brightness(130%)',
-    boxShadow: '1px 1px 6px',
-  },
-  padding: 0,
-};
-
 export const button = {
-  display: { xs: 'none', ml: 'none', lg: 'block' },
+  display: { xs: 'none', sm: 'block' },
   fontSize: '20px',
   width: '155px',
   height: '50px',
