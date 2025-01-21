9f21eaf211c07bd387e83c757f6bfd36ea42e052
German1138 <germancourvoisier1138@gmail.com>
2025-01-21 18:53:48 -0300

    feat: :speech_balloon: INFO_BUTTON_DATA constant added

---
+++ src/constants/INFO_BUTTON_DATA.js
@@ -0,0 +1,35 @@
+const INFO_BUTTON_DATA = {
+  title: 'Explanation of the Process',
+  description:
+    'Dediro uses artificial intelligence (AI) to filter, analyze, and select the highest quality content for users. Our approach combines a rigorous assessment based on various criteria that ensure the content includes only the best sources and viewpoints.',
+  accordion: [
+    {
+      title: 'Included Content',
+      bullets: [
+        `Authority and Credibility: We select content written or produced by recognized experts, thought leaders, or highly respected sources in their field. This content is backed by solid research and verifiable sources.`,
+        `Depth and Context: The content must provide detailed and well-contextualized analysis of the topic. It's not just about superficial facts; the topic should be addressed comprehensively, providing context that allows for a better understanding of the more complex details.`,
+        `Diverse Perspectives: We ensure the inclusion of different viewpoints on the same topic. We value the diversity of opinions to give users a more balanced perspective, even on topics where there is disagreement among experts.`,
+        `Intellectual Rigor and Critical Thinking: The content should promote critical thinking, challenge assumptions, and encourage deep understanding. Pieces that offer well-founded arguments and promote reflection are prioritized.`,
+      ],
+      description:
+        'Reason for Inclusion: We want to ensure that users receive accurate, well-researched, and high-quality information that not only informs but also promotes a deeper and more nuanced understanding of topics. By providing multiple and well-supported perspectives, we help users form informed and balanced opinions.',
+    },
+    {
+      title: 'Excluded Content',
+      bullets: [
+        `Lack of Reliable Sources: We exclude content that is not backed by verifiable sources or lacks credibility. If the author is not a recognized expert or the content is not based on solid facts, it is discarded.`,
+        `Excessive Simplicity: Content that reduces complex topics to simplistic explanations, without delving into the nuances or providing critical analysis, is excluded. This includes content that offers quick answers without considering the inherent complexity of the topic.`,
+        `Excessive Bias or Sensationalism: If the content is excessively biased or sensationalist, intended only to
+        generate clicks or emotions without offering an objective understanding, we exclude it. This also applies to material that reinforces echo chambers or is biased without providing a fair analysis.`,
+        `Repetition or Obsolete Information: Content that simply repeats what has already been covered without adding new insights or that is outdated and does not reflect the current reality is excluded.`,
+      ],
+      description:
+        'Reason for Exclusion: We exclude this type of content to ensure that the information reaching users is objective, relevant, and promotes a deep understanding, avoiding misinformation, superficiality, or content that does not add value. We want to protect our users from low-quality content that can generate misinformation or misunderstandings about complex topics.',
+    },
+  ],
+  subtitle: 'Rationale',
+  subDescription:
+    'The content selection process at Dediro aims to offer users only accurate and in-depth information based on high-quality sources. What we include is information backed by experts, well-contextualized, and that fosters critical thinking. What we exclude is content that does not meet these standards of intellectual rigor, diversity of perspectives, and credibility.',
+};
+
+export default INFO_BUTTON_DATA;
