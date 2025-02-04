e1d46dfc432a1fba1f1eba6d6042c204715f0652
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 17:09:35 -0300

    feat: :sparkles: Updated image logic. Improved error handling.
    Also moved the interfaces into a separed file

--- src/components/SynthesisPageComponents/ThemesSection/SourcesModal/ModalCard.tsx
+++ src/components/SynthesisPageComponents/ThemesSection/SourcesModal/ModalCard.tsx
@@ -1,35 +1,23 @@
 import { Box, Link, Typography } from '@mui/material';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import React from 'react';
 import {
   container,
   expertName,
   expertProfession,
-  image,
+  imageStyle,
   informationContainer,
   sourceLink,
   sourceSummary,
 } from './ModalCard.style';
 
-interface ISources {
-  quote: string;
-  link: string;
-  sourceName: string;
-  sourceDescription: string;
-}
+import { IModalCard } from '@/types';
+import React from 'react';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
-interface IModalCard {
-  id: number;
-  name: string;
-  profession: string;
-  sources: ISources[];
-}
-
-function ModalCard({ id, name, profession, sources }: IModalCard) {
+function ModalCard({ id, name, profession, sources, image }: IModalCard) {
   return (
     <Box key={id} sx={container}>
-      <Box sx={image}>
-        <img src="images/synthesis-page/modal/source-modal.png" alt="expert" />
+      <Box sx={imageStyle}>
+        <img src={image.src} alt={image.alt} />
       </Box>
       <Box sx={informationContainer}>
         <Typography variant="h2" sx={expertName}>
@@ -38,14 +26,18 @@ function ModalCard({ id, name, profession, sources }: IModalCard) {
         <Typography variant="h4" sx={expertProfession}>
           {profession}
         </Typography>
-        <Typography fontSize={14}>{sources[0].quote}</Typography>
-        <Link href={sources[0].link} underline="none" sx={sourceLink}>
-          <YouTubeIcon />
-          <Typography fontSize={10}>{sources[0].sourceName}</Typography>
-        </Link>
-        <Typography sx={sourceSummary}>
-          {sources[0].sourceDescription}
-        </Typography>
+        {sources && (
+          <>
+            <Typography fontSize={14}>{sources[0].quote}</Typography>
+            <Link href={sources[0].link} underline="none" sx={sourceLink}>
+              <YouTubeIcon />
+              <Typography fontSize={10}>{sources[0].sourceName}</Typography>
+            </Link>
+            <Typography sx={sourceSummary}>
+              {sources[0].sourceDescription}
+            </Typography>
+          </>
+        )}
       </Box>
     </Box>
   );
