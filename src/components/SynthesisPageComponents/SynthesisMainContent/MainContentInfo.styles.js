9b53a55f94993b85b9edd99ca1c51c47cf11aa55
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 17:01:38 -0300

    feat: :lipstick: Added MainContentInfo style file

---
+++ src/components/SynthesisPageComponents/SynthesisMainContent/MainContentInfo.styles.js
@@ -0,0 +1,24 @@
+export const articleInfo = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '30px',
+  marginBottom: '15px',
+};
+
+export const textTitleContainer = {
+  display: 'flex',
+  alignItems: 'center',
+};
+export const textTitle = { fontWeight: 700, fontSize: '20px' };
+
+export const textIcon = {
+  display: 'flex',
+  gap: '10px',
+  alignItems: 'center',
+};
+
+export const synthesisSummary = {
+  width: '100%',
+  textAlign: 'justify',
+  fontSize: '20px',
+};
