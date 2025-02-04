75074a573a0f19109b4ad8c27ab7e38a8a0cf8cc
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 16:48:30 -0300

    refactor: :recycle: Moved constants array into separated file

--- src/components/SynthesisPageComponents/SynthesisFilters/SynthesisFilters.tsx
+++ src/components/SynthesisPageComponents/SynthesisFilters/SynthesisFilters.tsx
@@ -1,41 +1,10 @@
 'use client';
 
-import React, { useState, useCallback } from 'react';
 import { Box, Button, useMediaQuery } from '@mui/material';
+import React, { useCallback, useState } from 'react';
+import { btnStylesMobile, container } from './SynthesisFilters.styles';
 
-import WidgetsRoundedIcon from '@mui/icons-material/WidgetsRounded';
-import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
-import MemoryRoundedIcon from '@mui/icons-material/MemoryRounded';
-import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import SportsSoccerOutlinedIcon from '@mui/icons-material/SportsSoccerOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
-import CheckroomOutlinedIcon from '@mui/icons-material/CheckroomOutlined';
-
-import { container, btnStylesMobile } from './SynthesisFilters.styles';
-
-const buttonData = [
-  { name: 'for-you', label: 'For you', icon: <WidgetsRoundedIcon /> },
-  { name: 'top', label: 'Top', icon: <StarBorderRoundedIcon /> },
-  {
-    name: 'tech-science',
-    label: 'Tech & Science',
-    icon: <MemoryRoundedIcon />,
-  },
-  { name: 'finance', label: 'Finance', icon: <PaidOutlinedIcon /> },
-  {
-    name: 'arts-culture',
-    label: 'Arts & Culture',
-    icon: <ColorLensOutlinedIcon />,
-  },
-  { name: 'sports', label: 'Sports', icon: <SportsSoccerOutlinedIcon /> },
-  { name: 'health', label: 'Health', icon: <FavoriteBorderOutlinedIcon /> },
-  { name: 'education', label: 'Education', icon: <SchoolOutlinedIcon /> },
-  { name: 'food', label: 'Food', icon: <FastfoodOutlinedIcon /> },
-  { name: 'fashion', label: 'Fashion', icon: <CheckroomOutlinedIcon /> },
-];
+import FILTERS_DATA from '../../../constants/FILTERS_DATA';
 
 function SynthesisFilters() {
   const [activeFilter, setActiveFilter] = useState('for-you');
@@ -47,7 +16,7 @@ function SynthesisFilters() {
 
   return (
     <Box sx={container}>
-      {buttonData.map(({ name, label, icon }) => (
+      {FILTERS_DATA.map(({ name, label, icon }) => (
         <Button
           key={name}
           variant={activeFilter === name ? 'contained' : 'text'}
