a0eaadfd4cb0803a3fe014185b9df21b601c164a
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 17:20:26 -0300

    feat: :goal_net: Improved error handling

--- src/components/SynthesisPageComponents/WeeklyUpdates/WeeklyUpdates.tsx
+++ src/components/SynthesisPageComponents/WeeklyUpdates/WeeklyUpdates.tsx
@@ -1,7 +1,7 @@
 import { Box, Grid2, Typography } from '@mui/material';
-import React from 'react';
-
 import { container, weeklyContentH3 } from './WeeklyUpdates.styles';
+
+import React from 'react';
 import WeeklyUpdatesCard from './WeeklyUpdatesCard';
 
 const WEEKLY_UPDATES_DATA = [
@@ -35,17 +35,19 @@ function WeeklyUpdates() {
       </Typography>
 
       <Grid2 container spacing={2}>
-        {WEEKLY_UPDATES_DATA?.map(({ articleTitle, image, link }, index) => {
-          return (
-            <Grid2 key={index} size={6}>
-              <WeeklyUpdatesCard
-                articleTitle={articleTitle}
-                image={image}
-                link={link}
-              />
-            </Grid2>
-          );
-        })}
+        {WEEKLY_UPDATES_DATA?.slice(0, 4).map(
+          ({ articleTitle, image, link }, index) => {
+            return (
+              <Grid2 key={index} size={6}>
+                <WeeklyUpdatesCard
+                  articleTitle={articleTitle}
+                  image={image}
+                  link={link}
+                />
+              </Grid2>
+            );
+          }
+        )}
       </Grid2>
     </Box>
   );
