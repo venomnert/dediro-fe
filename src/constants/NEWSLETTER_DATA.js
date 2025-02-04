b8b7be19fa648f52e3c7bf7790a5b0e49c16bfe2
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 17:11:57 -0300

    feat: :speech_balloon: Added constant for the NewsLetter text

---
+++ src/constants/NEWSLETTER_DATA.js
@@ -0,0 +1,6 @@
+const NEWSLETTER_DATA = {
+  title: 'Stay Informed about the Latest News on this Topic',
+  text: 'We continuously update our content to reflect new findings and perspectives. Keep up with the latest articles and reports from Dediro.',
+};
+
+export default NEWSLETTER_DATA;
