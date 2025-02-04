c680702194dd4f6fc5dbf31ce268121c03e1987e
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 16:43:19 -0300

    feat: :boom: Removed constant array
    Moved the data structure into a separated file. Also reordered the imports. Now the components gets the information from the page.tsx

--- src/components/SynthesisPageComponents/RelatedContent/RelatedContent.tsx
+++ src/components/SynthesisPageComponents/RelatedContent/RelatedContent.tsx
@@ -1,35 +1,16 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
-
 import {
-  container,
-  relatedContentH3,
-  moreArticlesSpan,
   cardContainer,
+  container,
+  moreArticlesSpan,
+  relatedContentH3,
 } from './RelatedContent.styles';
+
+import { IRelatedContentArray } from '@/types';
+import React from 'react';
 import RelatedContentCard from './RelatedContentCard';
 
-const RELATED_CONTENT_DATA = [
-  {
-    title: 'Related Article Title',
-    briefDescription:
-      'This is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related article.',
-  },
-  {
-    title: 'Related Article Title',
-    briefDescription: 'This is a brief description of the related article.',
-  },
-  {
-    title: 'Related Article Title',
-    briefDescription: 'This is a brief description of the related article.',
-  },
-  {
-    title: 'Related Article Title',
-    briefDescription: 'This is a brief description of the related article.',
-  },
-];
-
-function RelatedContent() {
+function RelatedContent({ relatedContent }: IRelatedContentArray) {
   return (
     <Box sx={container}>
       <Typography variant="h3" sx={relatedContentH3}>
@@ -40,7 +21,7 @@ function RelatedContent() {
         More Articles
       </Typography>
       <Box sx={cardContainer}>
-        {RELATED_CONTENT_DATA?.map(({ title, briefDescription }, index) => {
+        {relatedContent?.map(({ title, briefDescription }, index) => {
           return (
             <RelatedContentCard
               key={index}
