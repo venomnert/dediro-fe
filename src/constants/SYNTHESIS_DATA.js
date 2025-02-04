b561da51dae1ef64639478ab1b49549737468e7b
German1138 <germancourvoisier1138@gmail.com>
2025-02-04 17:14:13 -0300

    feat: :boom: Added all Synthesis boilerplate information into this file
    This file provides all the boilerplate information that the synthesis components use. Later we will use this as an example on how to implement the real data.

--- src/constants/SYNTHESIS_DATA.js
+++ src/constants/SYNTHESIS_DATA.js
@@ -1,6 +1,13 @@
 const SYNTHESIS_DATA = {
   title: 'How to live a happier life',
   slug: 'how-to-live-a-happier-life',
+  image: {
+    src: 'images/synthesis-page/main-content.png',
+    alt: 'happy blonde girl',
+  },
+  date: 'April 15, 2023',
+  summary:
+    'The pursuit of a happier life is a multifaceted endeavor shaped by various themes, including work-life balance, physical health, purpose, personal growth, gratitude, positive relationships, and mental well-being. Historically, the concept of work-life balance emerged from labor unions advocacy for reasonable working hours, evolving into modern discussions emphasizing emotional understanding and social networks as critical factors for happiness. Notably, Arthur C. Brooks highlights the necessity of metacognition and the role of relationships in achieving well-being, aligning with research showing the significant impact of social connections on career satisfaction. Flexible work arrangements, as studied by Phyllis Moen, further illustrate the shift towards work environments that support personal lives, enhancing overall happiness. Physical health, comprising regular exercise, a balanced diet, and sufficient sleep, is foundational to emotional well-being.',
   themesSection: [
     {
       id: 1,
@@ -66,6 +73,179 @@ const SYNTHESIS_DATA = {
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iste similique atque assumenda minima, quidem natus officia ut culpa, architecto ducimus labore nihil nesciunt! Fuga, inventore earum! Consequatur, eius reiciendis.',
         },
       ],
+    },
+  ],
+  experts: [
+    {
+      id: 1,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 7,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+    {
+      id: 8,
+      name: 'Bill Gates',
+      profession: 'American businessman',
+      image: {
+        src: 'images/synthesis-page/experts/bill-gates.png',
+        alt: 'bill gates profile picture',
+      },
+      sources: [
+        {
+          quote:
+            '1. Art of Happiness Course: Nurturing Positive Relationships and Social Support',
+          link: 'https://www.youtube.com/',
+          sourceName: 'YouTube',
+          sourceDescription:
+            'This video highlights the importance of nurturing positive relationships and social support for overall well-being, explaining how these connections contribute to emotional support, resilience, enhanced well-being, and shared experiences of joy.',
+        },
+      ],
+    },
+  ],
+  relatedContent: [
+    {
+      title: 'Related Article Title',
+      briefDescription:
+        'This is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related articleThis is a brief description of the related article.',
+    },
+    {
+      title: 'Related Article Title',
+      briefDescription: 'This is a brief description of the related article.',
+    },
+    {
+      title: 'Related Article Title',
+      briefDescription: 'This is a brief description of the related article.',
+    },
+    {
+      title: 'Related Article Title',
+      briefDescription: 'This is a brief description of the related article.',
     },
   ],
 };
