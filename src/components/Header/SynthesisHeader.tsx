586e6b24f06ef6d421d018653b7182ffcbb799d2
German1138 <germancourvoisier1138@gmail.com>
2025-02-11 17:44:59 -0300

    feat: :sparkles: Logo redirects to home

--- src/components/Header/SynthesisHeader.tsx
+++ src/components/Header/SynthesisHeader.tsx
@@ -11,11 +11,15 @@ import {
 import Image from 'next/image';
 import React from 'react';
 import SynthesisSearchbar from './SynthesisSearchbar';
+import { useRouter } from 'next/navigation';
 
 function SynthesisHeader() {
+  const router = useRouter();
+
   return (
     <Box sx={container}>
       <Image
+        onClick={() => router.push('/')}
         src="images/dediro-logo-blue.png"
         width={63}
         height={62}
